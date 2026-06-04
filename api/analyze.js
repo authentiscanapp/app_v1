b2bce9f7a509d6e29b8bccb092aa9576a83a0769
authentiscanapp <biancacosta@authentiscanapp.com>
2026-06-04 08:39:31 -0700

    Update analyze.js
    Fix: convert analyze.js to CommonJS + move supabase to dependencies

--- api/analyze.js
+++ api/analyze.js
@@ -1,6 +1,6 @@
-import { put, del } from "@vercel/blob";
+const { put, del } = require("@vercel/blob");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "*");
